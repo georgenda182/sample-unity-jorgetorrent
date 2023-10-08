40f35f793351c5fdb46a134c24ce2cf97e2fc049
georgenda182 <georgenda182@nicoteamstudios.com>
2023-10-08 23:19:49 +0200

    - Added Xbox input icons - Implemented primitive logic of interaction input icon displayement

--- _Unity_SampleJorgeTorrent/Assets/_SampleJorgeTorrent/Code/Characters/NPCs/SpeakerNPC.cs
+++ _Unity_SampleJorgeTorrent/Assets/_SampleJorgeTorrent/Code/Characters/NPCs/SpeakerNPC.cs
@@ -1,14 +1,20 @@
 using _SampleJorgeTorrent.Code.DialogSystem;
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace _SampleJorgeTorrent.Code.Characters.NPCs
 {
     [RequireComponent(typeof(SphereCollider))]
     public class SpeakerNPC : DialogsDispatcher
     {
+        [SerializeField] private Image _interactionMark;
         [SerializeField] private string[] _texts;
 
+        private Tween _interactionMarkVisibilityTween;
+
         private bool _canSpeak;
+        private bool _interactionMarkVisibilityTweenPlaying;
 
         protected override void DefineDialogs()
         {
@@ -32,14 +38,60 @@ namespace _SampleJorgeTorrent.Code.Characters.NPCs
             }
         }
 
+        private void Update()
+        {
+            if (_canSpeak)
+            {
+
+            }
+        }
+
         private void OnTriggerEnter(Collider other)
         {
             _canSpeak = true;
+            ShowInteractionMark();
+        }
+
+        private void ShowInteractionMark()
+        {
+            if (_interactionMarkVisibilityTweenPlaying)
+            {
+                _interactionMarkVisibilityTween.Complete(false);
+            }
+            _interactionMarkVisibilityTween = _interactionMark.DOFade(1, 0.35f);
+            _interactionMarkVisibilityTween.onPlay = delegate
+            {
+                _interactionMarkVisibilityTweenPlaying = true;
+                _interactionMark.gameObject.SetActive(true);
+            };
+            _interactionMarkVisibilityTween.onComplete = delegate
+            {
+                _interactionMarkVisibilityTweenPlaying = false;
+            };
         }
 
         private void OnTriggerExit(Collider other)
         {
             _canSpeak = false;
+            HideInteractionMark();
+        }
+
+        private void HideInteractionMark()
+        {
+            if (_interactionMarkVisibilityTweenPlaying)
+            {
+                _interactionMarkVisibilityTween.Complete(false);
+            }
+            _interactionMarkVisibilityTween = _interactionMark.DOFade(0, 0.35f);
+            _interactionMarkVisibilityTween.onPlay = delegate
+            {
+                _interactionMarkVisibilityTweenPlaying = true;
+            };
+            _interactionMarkVisibilityTween.onComplete = delegate
+            {
+                _interactionMarkVisibilityTweenPlaying = false;
+                _interactionMark.gameObject.SetActive(false);
+            };
         }
     }
 }
